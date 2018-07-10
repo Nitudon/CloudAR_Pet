479244fee5b4f6663a3937a0f27047a018113cc3
udon <naoki_udon@outlook.jp>
2018-07-11 01:37:44 +0900

    create load system

---
+++ Assets/Scripts/Utilities/ResourceLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 各種リソースの読み込み機構
+/// </summary>
+namespace CloudPet.Common.Resource
+{
+    public static class WorldObjectLoader
+    {
+        public static IPrefabLoader Loader;
+
+        public static T Load<T>(WorldObjectType type, Action<T> initialize = null) where T : UnityEngine.Object
+        {
+            var path = GetPrefabPath(type);
+            var obj = Resources.Load<T>(path);
+            initialize?.Invoke(obj);
+
+            return obj;
+        }
+
+        private static string GetPrefabPath(WorldObjectType type)
+        {
+            
+        }
+    }
+
+    public enum WorldObjectType
+    {
+        // プレイヤーキャラ
+        PlayerCharacter,
+    }
+
+    public interface IPrefabLoader
+    {
+        T Load<T>() where T : Object;
+    }
+}
