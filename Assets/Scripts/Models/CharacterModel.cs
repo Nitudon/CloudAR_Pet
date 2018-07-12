1c3439039a48c90151fc8901d07c7ba9e767fb2e
udon <naoki_udon@outlook.jp>
2018-07-12 23:56:49 +0900

    create charactermodel

---
+++ Assets/Scripts/Models/CharacterModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+
+public class CharacterModel : UdonBehaviour
+{
+
+    private string _characterName;
+
+
+}
