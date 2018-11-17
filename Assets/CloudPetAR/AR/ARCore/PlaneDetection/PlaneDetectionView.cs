edbbf0a7dadeb344031e930f7411ab5d1f8a1660
udon <myudongen@gmail.com>
2018-11-17 22:45:35 +0900

    plane detection

---
+++ Assets/CloudPetAR/AR/ARCore/PlaneDetection/PlaneDetectionView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+
+namespace CloudPet.AR
+{
+    public class PlaneDetectionView : UdonBehaviour
+    {
+        [SerializeField]
+        private GameObject _detectionMarker;
+    }
+}
