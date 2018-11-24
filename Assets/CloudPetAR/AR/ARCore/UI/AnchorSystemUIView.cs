d3eb035dbcb2ec689b53f969ec6783b4d9b44601
udon <myudongen@gmail.com>
2018-11-25 03:06:43 +0900

    breeder UI Parts

---
+++ Assets/CloudPetAR/AR/ARCore/UI/AnchorSystemUIView.cs
@@ -0,0 +1,32 @@
+﻿using UniRx;
+using UdonLib.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using CloudPet.AR;
+
+namespace CloudPet.AR
+{
+    /// <summary>
+    /// アンカー周りのUI
+    /// </summary>
+    public class AnchorSystemUIView : UIMono
+    {
+        [SerializeField]
+        private CommonButtonBase _anchorSettingButton;
+        public CommonButtonBase AnchorSettingButton => _anchorSettingButton;
+
+        [SerializeField]
+        private Text _anchorSettingDescription;
+
+        public void SetEnable(bool enable)
+        {
+            _anchorSettingButton.SetEnable(enable);
+            _anchorSettingDescription.enabled = enable;
+        }
+
+        public void SetAnchorSettingDescription(string text)
+        {
+            _anchorSettingDescription.text = text;
+        }
+    }
+}
---
+++ Assets/CloudPetAR/CloudPet/Pet/UI/PetSystemUI.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UdonLib.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using CloudPet.AR;
+
+namespace CloudPet.Pet
+{
+    /// <summary>
+    /// ペット周りのUI
+    /// </summary>
+    public class PetSystemUIView : UIMono
+    {
+        [SerializeField]
+        private CommonButtonBase _petActivationButton;
+        public CommonButtonBase PetActivationButton => _petActivationButton;
+
+        public void SetEnable(bool enable)
+        {
+            _petActivationButton.SetEnable(enable);
+        }
+    }
+}
