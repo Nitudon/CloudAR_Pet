44905b9041961d554621e62fbe2f2c61ed636a44
udon <myudongen@gmail.com>
2018-11-28 01:24:08 +0900

    singleton room manager

---
+++ Assets/CloudPetAR/Network/RoomManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdonLib.Commons;
+
+
+namespace CloudPet.Network
+{
+    /// <summary>
+    /// ルーム関連のマネージャー
+    /// </summary>
+    public class RoomManager : UdonBehaviourSingleton<RoomManager>
+    {
+        private RoomModel _model;
+        public RoomModel Model => _model;
+
+        protected override bool IsDontDestroy => true;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _model = new RoomModel();
+        }
+    }
+}
